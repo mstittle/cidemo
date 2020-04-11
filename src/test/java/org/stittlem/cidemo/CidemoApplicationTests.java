aad57b5eae374dd19104176df80797f4b9ba1008
michael stittleburg <mstittleburg@gmail.com>
2020-04-11 15:24:16 -0400

    create demo app to test jenkins build

---
+++ src/test/java/org/stittlem/cidemo/CidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stittlem.cidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
